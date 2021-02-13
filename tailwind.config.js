9bf3a4cc0007e4d7f48760877f9f49872809f2d7
Florian Reisinger <reisi007@gmail.com>
2021-02-13 10:54:02 +0100

    Project generation

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './src/**/*.html',
+      './src/**/*.scss'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontWeight: ['hover', 'focus'],
+      fontFamily: {
+        sans: ["-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      },
+      colors: {
+        reisishot: {
+          DEFAULT: '#27ae60',
+          light: '#2ecc71'
+        },
+        reisishot_boudoir: {
+          DEFAULT: '#d3973b'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
