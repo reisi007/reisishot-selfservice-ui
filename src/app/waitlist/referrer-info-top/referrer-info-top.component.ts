89d585fd60db12845bce43a828e5b5dd97c9a9bc
Florian Reisinger <reisi007@gmail.com>
2021-10-17 10:58:30 +0200

    Add analytics event

--- src/app/waitlist/referrer-info-top/referrer-info-top.component.ts
+++ src/app/waitlist/referrer-info-top/referrer-info-top.component.ts
@@ -1,15 +1,20 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ExtMatomoTracker} from '../../commons/ExtMatomoTracker';
+import {MatomoTracker} from '@ngx-matomo/tracker';
 
 @Component({
   selector: 'app-referrer-info-top',
   templateUrl: './referrer-info-top.component.html',
   styleUrls: ['./referrer-info-top.component.scss'],
 })
-export class ReferrerInfoTopComponent {
+export class ReferrerInfoTopComponent implements OnInit {
 
   private _email!: string;
 
-  constructor() {
+  private matomo: ExtMatomoTracker;
+
+  constructor(matomo: MatomoTracker) {
+    this.matomo = new ExtMatomoTracker(matomo);
   }
 
   get referrer(): string {
@@ -18,6 +23,10 @@ export class ReferrerInfoTopComponent {
 
   @Input() set referrer(referrer: string) {
     this._email = referrer;
+  }
+
+  ngOnInit(): void {
+    this.matomo.trackEvent('referral', 'open', this.referrer);
   }
 
 }
