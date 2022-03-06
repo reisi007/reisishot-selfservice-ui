e7c4c89ce3028f0a7ecd556d76f204cf231e0c30
Florian Reisinger <reisi007@gmail.com>
2022-03-06 07:37:41 +0100

    ical-view (#34)
    * Reduce method complexity
    
    * Fix errors from rebase

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,20 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AdminLoginService} from './login/admin-login.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
   constructor(
     private adminLoginService: AdminLoginService,
+    private router: Router,
   ) {
   }
 
   get isAdminLoggedIn(): boolean {
     return this.adminLoginService.data != null;
+  }
+
+  ngOnInit(): void {
+    if (!this.isAdminLoggedIn) {
+      this.router.navigate(['/dashboard', 'login'], {replaceUrl: true});
+    }
   }
 
 }
