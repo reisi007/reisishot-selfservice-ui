731a8ff9ec2bc9fbf1fb99f8158a5ed5d70eaa79
Florian Reisinger <reisi007@gmail.com>
2022-04-03 10:05:14 +0200

    Added PWA ability (#60)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgxMatomoTrackerModule} from '@ngx-matomo/tracker';
 import {NgxMatomoRouterModule} from '@ngx-matomo/router';
 import {NgHttpCachingModule} from 'ng-http-caching';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,6 +25,12 @@ import {NgHttpCachingModule} from 'ng-http-caching';
     NgxMatomoRouterModule,
     NgHttpCachingModule.forRoot({
       lifetime: 60 * 1000, // 1 minute
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
     }),
   ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true}],
