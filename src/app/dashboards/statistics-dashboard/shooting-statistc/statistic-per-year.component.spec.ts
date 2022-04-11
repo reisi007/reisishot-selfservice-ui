1ddbe8a0bcc49da000015437e709067a5c24c533
Florian Reisinger <reisi007@gmail.com>
2022-04-11 17:34:56 +0200

    Add Soll / Ist statistics and made a test

--- src/app/dashboards/statistics-dashboard/shooting-statistc/statistic-per-year.component.spec.ts
+++ src/app/dashboards/statistics-dashboard/shooting-statistc/statistic-per-year.component.spec.ts
@@ -1,6 +1,9 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-
+import {HttpClientModule} from '@angular/common/http';
 import {StatisticPerYearComponent} from './statistic-per-year.component';
+import {ShootingStatisticApiService} from '../api/shooting-statistic-api.service';
+import {AdminLoginService} from '../../../dashboard/login/admin-login.service';
+import {ReactiveFormsModule} from '@angular/forms';
 
 describe('ShootingStatistcComponent', () => {
   let component: StatisticPerYearComponent;
@@ -8,7 +11,9 @@ describe('ShootingStatistcComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+                   imports: [HttpClientModule, ReactiveFormsModule],
                    declarations: [StatisticPerYearComponent],
+                   providers: [ShootingStatisticApiService, AdminLoginService],
                  })
                  .compileComponents();
   });
@@ -21,5 +26,14 @@ describe('ShootingStatistcComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(('sum of expectedPercentages should be 100'), () => {
+    const sum = Object.values(StatisticPerYearComponent.overrides)
+                      .map(e => e.expectedPercentage)
+                      .filter(e => e > 0)
+                      .reduce((a, b) => a + b);
+
+    expect(sum).toBeCloseTo(100, 0.0001);
   });
 });
