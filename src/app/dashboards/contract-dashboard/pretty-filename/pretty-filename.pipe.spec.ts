295f76ad2d3f288a897dc8c56abd73713685b695
Florian Reisinger <reisi007@gmail.com>
2022-05-03 19:16:09 +0200

    Removed almost all specs + improved CI build

--- src/app/dashboards/contract-dashboard/pretty-filename/pretty-filename.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import {PrettyFilenamePipe} from './pretty-filename.pipe';
-
-describe('PrettyFilenamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PrettyFilenamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
